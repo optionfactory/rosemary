07dea771bf739920c7dad4437e2a0fbe680e713f
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2019-07-03 02:00:33 +0200

    :fix: strange import

--- src/main/java/net/optionfactory/rosemary/AppendVersionToResource.java
+++ src/main/java/net/optionfactory/rosemary/AppendVersionToResource.java
@@ -1,6 +1,5 @@
 package net.optionfactory.rosemary;
 
-import com.sun.tools.javac.resources.version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.context.ITemplateContext;
